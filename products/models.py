b975b1351afe5daade4514546e777b3649090753
ReynaldiRP <rizkypratama471@gmail.com>
2026-01-31 14:59:23 +0700

    Initialize Django project structure with essential files and configurations

---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Status(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
